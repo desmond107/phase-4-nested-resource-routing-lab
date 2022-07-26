ccaeeaf6b46985333fd5ce1c5880ec6ac06778fd
desmond <desmond@desmonds-MacBook-Pro.local>
2022-07-26 10:37:28 +0300

    task complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
     render json: user, include: :items
   end
 
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end  
+
 end
